8472ecef0c775394f0c478b12389d86c75080e8d
Alberto Núñez García <UO258455@uniovi.es>
2021-10-16 17:21:52 +0200

    navegacion entre activities

--- app/src/main/java/com/uniovi/mytasks/FormularioActivity.java
+++ app/src/main/java/com/uniovi/mytasks/FormularioActivity.java
@@ -2,9 +2,14 @@ package com.uniovi.mytasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.mytasks.R;
+import com.uniovi.mytasks.modelo.Task;
+
+import java.util.Date;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -12,5 +17,22 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button buttonOk = findViewById(R.id.buttonOk);
+        Button buttonCancel = findViewById(R.id.buttonCancel);
+
+        buttonOk.setOnClickListener(view -> {
+            /** TODO: Obtener datos correctos de la tarea **/
+            Task task = new Task("","",new Date(System.currentTimeMillis()));
+            Intent intentResult = new Intent();
+            intentResult.putExtra(MainActivity.TAREA_ADD,task);
+            setResult(RESULT_OK,intentResult);
+            finish();
+        });
+
+        buttonCancel.setOnClickListener(view ->{
+                setResult(RESULT_CANCELED);
+                finish();
+        });
     }
 }
--- app/src/main/java/com/uniovi/mytasks/MainActivity.java
+++ app/src/main/java/com/uniovi/mytasks/MainActivity.java
@@ -9,13 +9,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.mytasks.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.uniovi.mytasks.modelo.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public final static int GESTION_TAREA = 1;
-
+    public final static String TAREA_ADD = "tarea_add";
     List<Task> listaTareas;
     /**
      * TODO: Pendiente de implementar vista para poder ver las tareas a modo de lista y pulsar en ellas
@@ -26,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cargarTareas();
+
+        FloatingActionButton fab = findViewById(R.id.botonAdd);
+        fab.setOnClickListener(view ->{
+            crearNuevaTarea();
+        });
     }
 
     private void crearNuevaTarea(){
@@ -33,12 +42,20 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent,GESTION_TAREA);
     }
 
+    private void cargarTareas(){
+        listaTareas = new ArrayList<Task>();
+    }
+
+    //private void listaTareasAdapter(){}
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == GESTION_TAREA){
             if(resultCode == RESULT_OK){
-
+                Task task = data.getParcelableExtra(TAREA_ADD);
+                listaTareas.add(task);
+                //listaTareasAdapter()
             }
         }else if(resultCode == RESULT_CANCELED)
             Log.d("MyTasks.MainActivity","FormularioActivity cancelada");
